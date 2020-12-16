224acf6cc209d78376f933b6b1d839d676e1790e
Agnieszka Polińska <agapoli@gmail.com>
2020-12-16 16:28:58 +0100

    class Main

---
+++ app/Main.js
@@ -0,0 +1,12 @@
+class Main {
+    htmlElements = {
+        addInput: document.getElementById('js-add-product-input'),
+        addBtn: document.getElementById('js-add-product-btn'),
+        addUl: document.getElementById('js-add-product-ul'),
+        addInfo: document.getElementById('js-add-product-ul'),
+        listImg: document.getElementById('js-list-panel--start'),
+        listProducts: document.getElementById('js-list-panel--with-products'),
+        numberToBuy: document.getElementById('js-number-to-buy'),
+        numberInBasket: document.getElementById('js-number-in-basket'),
+    }
+}
