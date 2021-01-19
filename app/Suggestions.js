4791dd46cd669ffbb0103a209c24bdb1a4dce439
Agnieszka Polińska <agapoli@gmail.com>
2021-01-19 13:03:21 +0100

    implement methods: searchProduct and chooseProduct

--- app/Suggestions.js
+++ app/Suggestions.js
@@ -1,14 +1,45 @@
 import { LocalStorage } from "./LocalStorage.js";
 
 export class Suggestions {
+    htmlElements = {
+        searchUl: document.getElementById('js-add-product-ul'),
+        btn: document.getElementById('js-add-product-btn'),
+    }
     constructor() {
         const _listOfSuggestions = localStorageObj.getSuggestions();
         this.showSuggestions = () => _listOfSuggestions;
     }
     searchProduct(event) {
-        console.log(event.target.value);
+        const searchedWord = event.target.value.toLowerCase();
+        const allSuggestions = this.showSuggestions();
+        console.log(searchedWord);
+        console.log(allSuggestions);
         
+        const matchingSugg = allSuggestions.filter(suggestion => suggestion.includes(searchedWord));
+        console.log(matchingSugg);
+        
+        matchingSugg.forEach(suggestion => {
+            const suggLi = document.createElement('li');
+            suggLi.classList.add('add-product-panel__input');
+            suggLi.classList.add('add-product-panel__input--li');
+            suggLi.textContent = suggestion;
+            this.htmlElements.searchUl.appendChild(suggLi);
+            suggLi.addEventListener('click', this.chooseProduct);
+        });
+
+        console.log(window.innerWidth);
+        
+        if (window.innerWidth < 700) {
+            this.htmlElements.btn.classList.add('hidden')
+        }
     }
+
+    chooseProduct(e) {
+        document.getElementById('js-add-product-input').value = e.target.textContent;
+        document.getElementById('js-add-product-ul').innerHTML = "";
+        document.getElementById('js-add-product-btn').classList.remove('hidden');
+    }
+
 }
 
 const localStorageObj = new LocalStorage();
