5f29cb1f48fbaee4cbefe4ee2aa09d2e80d49e25
Agnieszka Polińska <agapoli@gmail.com>
2021-01-25 11:23:33 +0100

    add selectors to uiSelectors

--- app/UI.js
+++ app/UI.js
@@ -3,10 +3,12 @@ export class UI {
       input: 'js-add-product-input',
       btn: 'js-add-product-btn',
       inBasket: 'js-number-in-basket',
+      numberToBuy: 'js-number-to-buy',
       productsLS: '.list-panel--with-products li',
       divWithProducts: 'js-list-panel--with-products',
       divWithImage: 'js-list-panel--start',
       ulAdd: 'js-list-panel-ul',
+      ulSearch: 'js-add-product-ul',
   }  
   getElement(selector) {
       return document.getElementById(selector)
